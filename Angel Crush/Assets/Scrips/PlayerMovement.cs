705935e2511614a42a5f31fa826c260eb2ad902e
tamir yaffe <100083346+tamir-yaffe@users.noreply.github.com>
2023-09-23 15:49:38 +0300

    scripts
    added movement to player object
    added rigidbody

--- Angel Crush/Assets/Scrips/PlayerMovement.cs
+++ Angel Crush/Assets/Scrips/PlayerMovement.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+//(2.1,3.7)(-2.1,3.7)
 {
-    // Start is called before the first frame update
+    // GameObjects
+    public GameObject player;
+    // Variabels
+    float dir;
+    int speed;
     void Start()
     {
-        
+        speed = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dir = Input.GetAxis("Horizontal");
+        if (dir <= 1 && transform.position.x < 2.1f)
+        {
+            transform.Translate(dir * speed * Time.deltaTime, 0, 0);
+        }
+        else if (dir >= -1 && transform.position.x > -2.1f)
+        {
+            transform.Translate(dir * speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            dir = 0;
+        }
     }
 }
